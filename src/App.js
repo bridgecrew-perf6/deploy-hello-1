9341b5f1365b2a65e156dbc4abbcec84ab3fe27e
Triệu Hữu Định <dinhth@Macbooks-iMac.local>
2022-05-29 17:58:49 +0700

    fix react deploy

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ export default App;
 // 1 npm run build
 // 2 create new repo deploy-hello 
 // 3 npm install -D gh-pages
+
 // 4 package.json 
 // a. add "homepage": "https://{username}.github.io/{repo-name}",
-
 // b. add deploy và predeploy in "scripts"
 // "predeploy": "npm run build", // Build before deploy
 // "deploy": "gh-pages -d build" // Deploy the "build" folder
@@ -26,3 +26,6 @@ export default App;
 // git branch -M main
 // git remote add origin https://github.com/huudinh/deploy-hello.git
 // git push -u origin main
+
+// 6 npm run deploy
+// https://huudinh.github.io/deploy-hello
